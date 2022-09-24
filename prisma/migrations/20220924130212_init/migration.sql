d4027b0b05d15cac8bf581e663bbda4b4c6a1875
hamabek <hamabek@ikyu.com>
2022-09-24 22:04:16 +0900

    prisma 導入

---
+++ prisma/migrations/20220924130212_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
