52e6c1de3ce8be0dbfe9bc1d50fbc66a30049c2f
hamabek <hamabek@ikyu.com>
2022-09-25 11:04:10 +0900

    feat: jest環境構築

---
+++ tests/graphql/__helpers.ts
@@ -0,0 +1,40 @@
+import { ServerInfo } from 'apollo-server'
+import { GraphQLClient } from 'graphql-request'
+import getPort, { makeRange } from 'get-port'
+import { server } from '../../src/graphql/server'
+
+type TestContext = {
+  client: GraphQLClient
+}
+
+export function createTestContext(): TestContext {
+  let ctx = {} as TestContext
+  const graphqlCtx = graphqlTestContext()
+
+  beforeEach(async () => {
+    const client = await graphqlCtx.before()
+
+    ctx = { ...ctx, client }
+  })
+
+  afterEach(async () => {
+    await graphqlCtx.after()
+  })
+
+  return ctx
+}
+
+function graphqlTestContext() {
+  let serverInstance: ServerInfo | null = null
+
+  return {
+    async before() {
+      const port = await getPort({ port: makeRange(4000, 6000) })
+      serverInstance = await server.listen({ port })
+      return new GraphQLClient(`http://localhost:${port}`)
+    },
+    async after() {
+      serverInstance?.server.close()
+    }
+  }
+}
